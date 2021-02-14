d8bba5a2eb2a10400977692558c8163d125f7ec4
himanshunete <himanshuneteh@gmail.com>
2021-02-14 13:36:20 +0530

    [himanshunete]Add FirstCommit

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        public Node head ;
+        public Node lastNode;
+        /// <summary>
+        /// UC-2 Appending elements
+        /// </summary>
+        /// <param name="new_data"></param>
+//        public void Append(int new_data)
+//        {
+//            Node new_node = new Node(new_data);
+//            if (this.head != null)
+//            {
+//                lastNode = GetLastNode();
+//                lastNode.next = new_node;
+//            }
+//            else
+//            {
+//                this.head = new_node;
+//            }
+            //Console.WriteLine("Inserted into list " + new_node.data);
+//        }
+
+//        public Node GetLastNode()
+//        {
+//            Node temp = this.head;
+//            while (temp.next != null)
+//            {
+//                temp = temp.next;
+//            }
+//            return temp;
+//        }
+
+//        public void Add(int new_data)
+//        {
+//            Node new_node = new Node(new_data);
+//            new_node.next = this.head;
+//            this.head = new_node;
+
+//        }
+
+//        public void Display()
+//        {
+//            Console.WriteLine("Displaying Nodes");
+//            Node temp = this.head;
+//            if ( temp == null)
+//            {
+//                Console.WriteLine("LinkedList is empty");
+//            }
+//            else
+//            {
+//                while (temp != null)
+//                {
+//                    Console.WriteLine(" " + temp.data);
+//                    temp = temp.next;
+//                }
+//
+//            }
+//        }
+//    }
+//}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List");
+            LinkedList list = new LinkedList();
+        //  list.InsertLast(56);
+        //  list.InsertLast(30);
+        //  list.InsertLast(70);
+        //  list.Display();
+
+    //      list.InsertFront(70);
+    //      list.InsertFront(30);
+    //      list.InsertFront(56);
+    //      list.Display();
+        }
+    }
+}
